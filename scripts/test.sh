eefc228e386d4388424d8d8968722f55352bc9ce
onlytwokey <130299828+onlytwokey@users.noreply.github.com>
2026-07-17 14:22:45 +0800

    fix: support system Bash in CI

--- scripts/test.sh
+++ scripts/test.sh
@@ -6,19 +6,18 @@ TESTING_FRAMEWORKS="$DEVELOPER_DIR/Library/Developer/Frameworks"
 TESTING_MACROS="$DEVELOPER_DIR/usr/lib/swift/host/plugins/testing/libTestingMacros.dylib"
 TESTING_INTEROP="$DEVELOPER_DIR/Library/Developer/usr/lib"
 
-SWIFT_FLAGS=()
 if [[ -d "$TESTING_FRAMEWORKS/Testing.framework" && -f "$TESTING_MACROS" ]]; then
   export DYLD_FRAMEWORK_PATH="$TESTING_FRAMEWORKS${DYLD_FRAMEWORK_PATH:+:$DYLD_FRAMEWORK_PATH}"
-  SWIFT_FLAGS=(
-    -Xswiftc -F
-    -Xswiftc "$TESTING_FRAMEWORKS"
-    -Xswiftc -load-plugin-library
-    -Xswiftc "$TESTING_MACROS"
-    -Xlinker -rpath
-    -Xlinker "$TESTING_FRAMEWORKS"
-    -Xlinker -rpath
-    -Xlinker "$TESTING_INTEROP"
-  )
+  exec swift test \
+    -Xswiftc -F \
+    -Xswiftc "$TESTING_FRAMEWORKS" \
+    -Xswiftc -load-plugin-library \
+    -Xswiftc "$TESTING_MACROS" \
+    -Xlinker -rpath \
+    -Xlinker "$TESTING_FRAMEWORKS" \
+    -Xlinker -rpath \
+    -Xlinker "$TESTING_INTEROP" \
+    "$@"
 fi
 
-exec swift test "${SWIFT_FLAGS[@]}" "$@"
+exec swift test "$@"
